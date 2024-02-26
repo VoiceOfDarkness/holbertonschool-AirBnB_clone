bfd9b7ce724f57f3651d80084733ecbcbd42f2cb
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-26 17:28:01 +0400

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -4,28 +4,19 @@ from datetime import date, datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs) -> None:
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(
-                        self,
-                        key,
-                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
-                    )
-                elif key != "__class__":
-                    setattr(self, key, value)
-        else:   
-            self.id = str(uuid4())
-            self.created_at = datetime.now() 
-            self.updated_at = datetime.now()
+        self.id = str(uuid4())
+        self.created_at = datetime.now() 
+        self.updated_at = datetime.now()
 
     def save(self) -> None:
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         result = self.__dict__.copy()
         result["__class__"] = self.__class__.__name__
-        return str(result)
+        result["created_at"] = self.created_at.isoformat()
+        result["updated_at"] = self.updated_at.isoformat()
+        return result
 
     def __str__(self) -> str:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
