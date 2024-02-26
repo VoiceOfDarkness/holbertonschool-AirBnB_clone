ec060c094093189e7a9b342410367a6e37254dba
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-26 17:08:18 +0400

    initial commit

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs) -> None:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(
+                        self,
+                        key,
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
+                    )
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:   
+            self.id = str(uuid4())
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = datetime.now().isoformat()
+
+    def save(self) -> None:
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        result = self.__dict__.copy()
+        result["__class__"] = self.__class__.__name__
+        return result
+
+    def __str__(self) -> str:
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
