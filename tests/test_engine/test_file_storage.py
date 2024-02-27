bcb0114364e961748c6eb9634588d0873432edb9
Somed-1 <abbasovmaqa159@gmail.com>
2024-02-27 18:53:09 +0400

    Hope it will work

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -13,9 +13,18 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         self.storage = FileStorage()
         self.storage.reload()
+        self.base = BaseModel()
     
     def test_all(self):
         obj = self.storage.all()
         self.assertEqual(type(obj), dict)
         self.assertIs(obj, FileStorage._FileStorage__objects)
         self.assertEqual(len(obj), 7)
+
+    def test_new(self):
+        """Test new method."""
+        bm = BaseModel()
+        self.storage.new(bm)
+        store = FileStorage._FileStorage__objects
+        self.assertIn("BaseModel." + bm.id, store.keys())
+        self.assertIn(self.base, store.values())
