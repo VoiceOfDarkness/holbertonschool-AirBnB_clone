87c534300369c604a2d3052a80fc0d681d364144
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-28 18:33:29 +0400

    deleted else condition

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,10 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs) -> None:
         from models import storage
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -15,10 +19,6 @@ class BaseModel:
                     )
                 elif key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
         storage.new(self)
 
     def save(self) -> None:
