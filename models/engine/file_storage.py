2280c656931f07c2bc80187aa1d98978a85780f3
Somed-1 <abbasovmaqa159@gmail.com>
2024-02-28 15:54:13 +0400

    Add new classes to handle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,22 @@
 import json
 
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
 from models.user import User
+from models.review import Review
+from models.state import State
 
 
 classes = {
     "BaseModel": BaseModel,
     "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
 }
 
 class FileStorage:
