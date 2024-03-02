08adbe808e2c9eb4dc5a3d1730e1f12b5f98410e
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-02 14:07:04 +0400

    added docs for module and funcitons

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Test for console.py"""
 import os
 import unittest
 from io import StringIO
@@ -9,24 +11,34 @@ from models.engine.file_storage import FileStorage
 
 class TestHBNBCommand(unittest.TestCase):
     def setUp(self):
-        FileStorage._FileStorage__objects = {}
+        """Set up for the test"""
+
+        FileStorage.__objects = {}
         self.HBNB = HBNBCommand()
 
     def test_emptyline(self):
+        """Test for empty line"""
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("\n")
             self.assertEqual("", f.getvalue())
 
     def test_quit(self):
+        """Test for quit command"""
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("quit")
             self.assertEqual("", f.getvalue())
 
     def test_EOF(self):
+        """Test for EOF command"""
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.assertTrue(self.HBNB.onecmd("EOF"))
 
     def test_create_errors(self):
+        """Test for create command errors"""
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("create")
             self.assertEqual("** class name missing **\n", f.getvalue())
@@ -35,6 +47,8 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertEqual("** class doesn't exist **\n", f.getvalue())
 
     def test_show(self):
+        """Test for show command errors"""
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("show")
             self.assertEqual("** class name missing **\n", f.getvalue())
@@ -49,6 +63,8 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertEqual("** no instance found **\n", f.getvalue())
 
     def test_destroy(self):
+        """Test for destroy command errors"""
+
         with patch("sys.stdout", new=StringIO()) as f:
             self.HBNB.onecmd("destroy")
             self.assertEqual("** class name missing **\n", f.getvalue())
@@ -62,15 +78,9 @@ class TestHBNBCommand(unittest.TestCase):
             self.HBNB.onecmd("destroy BaseModel 12345")
             self.assertEqual("** no instance found **\n", f.getvalue())
 
-    def test_destroy(self):
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("Maga.destroy()")
-            self.assertEqual("** class doesn't exist **\n", f.getvalue())
-        with patch("sys.stdout", new=StringIO()) as f:
-            self.HBNB.onecmd("User.destroy(12345)")
-            self.assertEqual("** no instance found **\n", f.getvalue())
-
     def tearDown(self):
+        """Clean the test"""
+
         try:
             os.remove("file.json")
         except IOError:
